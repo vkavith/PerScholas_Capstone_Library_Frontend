38bb11017469a20bb0dc4a11d4226c7e0c26ceb1
vkavith <vkavith@gmail.com>
2025-02-05 16:28:10 -0500

    Render url given

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 //Have a common API , so change the URL in one place
 
-const API = "http://localhost:5000";
-
+//const API = "http://localhost:5000";
+const API = "https://perscholas-capstone-library-backend.onrender.com";
 export default API;
 
 //https://perscholas-capstone-library-backend.onrender.com
